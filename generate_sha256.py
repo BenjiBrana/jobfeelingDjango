34ef7ffb54baf450647b5ebf35fc28bcab6afd7d
BenjiBrana <115607309+BenjiBrana@users.noreply.github.com>
2024-09-01 23:37:17 +0200

    Delete generate_sha256.py

--- generate_sha256.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-import base64
-
-with open('htmx.min.js', 'rb') as f:
-    file_hash = hashlib.sha256(f.read()).digest()
-    base64_hash = base64.b64encode(file_hash).decode('utf-8')
-    print(base64_hash)
